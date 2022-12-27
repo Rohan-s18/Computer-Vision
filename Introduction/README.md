f3c195e31e511c603348470953b062c1cfcf0bf6
Rohan Singh <rohan.b.singh54@gmail.com>
2022-12-26 22:07:06 -0800

    Added module for capturing videos from webcam

--- Introduction/README.md
+++ Introduction/README.md
@@ -1,8 +1,10 @@
-# Introductory Project
+# Introduction
 ## Author: Rohan Singh
-This directory contains a simple open-cv source code using python which will acts as an introduction to Computer Vision.  
+This directory contains source code for simple Open CV GUI features using Python which will acts as an introduction to Computer Vision.  
 
 ## Content
 This directory contaoins the following code:  
   - **open_image.py:** This Python module contains the source code to open an image using open cv, displaying it in an opencv window, and saving it to a given location.  
+  - **capture_video.py:** This python module contains source code to Capture videos using OpenCV from a camera, in this case it will be the built-in web camera.
+
 
---
+++ Introduction/capture_video.py
@@ -0,0 +1,49 @@
+"""
+Author: Rohan Singh
+12/26/22
+This python module contains source code to Capture videos using OpenCV from a camera, in this case it will be the built-in web camera.
+"""
+
+#Imports
+import numpy as np
+import cv2 as cv
+
+
+#This function will capture the video
+def capture_video():
+
+    #Using the video capture function
+    cap = cv.VideoCapture(0)
+    if not cap.isOpened():
+        print("Cannot open camera")
+        exit()
+    
+    #Continously capture it
+    while True:
+        flag, frame = cap.read()
+        
+        #if frame is not read correctly flag is False
+        if flag == False:
+            print("uh oh")
+            break
+
+        #Display the web cam footage
+        cv.imshow("Your Video",frame)
+        if cv.waitKey(1) == ord('q'):
+            break
+    # When everything done, release the capture
+    cap.release()
+    cv.destroyAllWindows()
+
+
+#Main method
+def main():
+    #print("Hello World!")
+
+    #Calling the capture video function
+    capture_video()
+
+if __name__ == "__main__":
+    main()
+
+
